7e30dee6691782d8d11e8e6c8a2ec96147f91e3d
Lars <LarsJaud7@outlook.de>
2025-01-12 22:07:51 +0100

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Lars Jaud on 12.01.25.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
